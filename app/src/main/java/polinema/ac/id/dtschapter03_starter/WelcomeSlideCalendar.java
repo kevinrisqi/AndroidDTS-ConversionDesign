2ac869c145be7640538a97e7d21455d8aa541ec8
Kevin <kevinrisqi22@gmail.com>
2020-05-30 14:00:42 +0700

    Add Welcome Screen

--- app/src/main/java/polinema/ac/id/dtschapter03_starter/WelcomeSlideCalendar.java
+++ app/src/main/java/polinema/ac/id/dtschapter03_starter/WelcomeSlideCalendar.java
@@ -9,5 +9,7 @@ public class WelcomeSlideCalendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_slide_calendar);
+
+        getSupportActionBar().hide();
     }
 }
